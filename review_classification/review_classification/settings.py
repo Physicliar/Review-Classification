835bdb6e6a5165c09c27248d832eb8e2a65096e1
Emre Demir <44400095+Physicliar@users.noreply.github.com>
2021-11-29 14:00:34 +0300

    Update settings.py

--- review_classification/review_classification/settings.py
+++ review_classification/review_classification/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n*g4ojh13d^*tg@7*v^5lp81^dx%^3-g3$qqq9hvp-444)mpp5'
+# SECRET_KEY = 'django-insecure-n*g4ojh13d^*tg@7*v^5lp81^dx%^3-g3$qqq9hvp-444)mpp5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
